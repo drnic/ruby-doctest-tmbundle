f043b151265e2d6b6dd96c1d1af74aac7c645ca8
René Schwaiger <sanssecours@f-m.fm>
2016-08-21 15:36:10 +0200

    Run DocTest: Set working directory
    The command “Run DocTest” now set the working directory before it runs
    the `rubydoctest` command. The command uses
    
    - the current project directory (`TM_PROJECT_DIRECTORY`), or if this
      directory is not set,
    
    - it changes into the directory of the current file (`TM_FILEPATH`).
    
    This changes makes sure that doctests that rely on the working
    directory now run correctly.

--- Support/bin/run_doctest.rb
+++ Support/bin/run_doctest.rb
@@ -1,3 +1,5 @@
 #!/usr/bin/env ruby
 
-puts `rubydoctest --html --ignore-interactive "#{File.expand_path ENV['TM_FILEPATH']}"`
+filepath = File.expand_path ENV['TM_FILEPATH']
+Dir.chdir(ENV['TM_PROJECT_DIRECTORY'] || File.dirname(filepath))
+puts `rubydoctest --html --ignore-interactive "#{filepath}"`
